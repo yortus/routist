298d6364da7eb90af5a933f2fc0e3a478d6411b8
Troy Gerwien <yortus@gmail.com>
2016-01-13 16:16:06 +0800

    improve normalizeHandler tests

--- test/normalize-handler.js
+++ test/normalize-handler.js
@@ -1,19 +1,66 @@
-// 'use strict';
-// import {expect} from 'chai';
-// import normalizeHandler from '../src/normalize-handler';
-// 
-// 
-// describe('normalizeHandler', () => {
-// 
-// 
-//     it('works', () => {
-// 
-//         let h2 = normalizeHandler('/foo/{...path}/{name}.{ext}', (function (path, name, req, ext) {
-//             return null;
-//         }));
-// 
-//         let a = h2({ pathname: '/foo/bar/baz.html'});
-// 
-//     });
-// });
+'use strict';
+var chai_1 = require('chai');
+var normalize_handler_1 = require('../src/normalize-handler');
+describe('normalization of a handler function', () => {
+    let tests = [
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'rest',
+            handlerArgValues: 'foo/bar/baz.html'
+        },
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'req, rq, rest',
+            handlerArgValues: '[object Object], [object Object], foo/bar/baz.html'
+        },
+        {
+            pattern: '/api/…',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: '',
+            handlerArgValues: ''
+        },
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: '',
+            handlerArgValues: 'ERROR'
+        },
+        {
+            pattern: '/api/…',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'rest',
+            handlerArgValues: 'ERROR'
+        },
+        {
+            pattern: '/foo/{...path}/{name}.{ext}',
+            pathname: '/foo/bar/baz.html',
+            handlerParamNames: 'path, name, req, ext',
+            handlerArgValues: 'bar, baz, [object Object], html'
+        }
+    ];
+    tests.forEach((test, i) => {
+        it(`${test.pattern} WITH (${test.handlerParamNames}) => {...}`, () => {
+            let handler = makeHandlerFunction(test.handlerParamNames);
+            let canonicalHandler = normalize_handler_1.default(test.pattern, handler);
+            let request = { pathname: test.pathname };
+            let expectedArgs = test.handlerArgValues;
+            let actualArgs = 'ERROR';
+            try {
+                actualArgs = canonicalHandler(request);
+            }
+            catch (ex) { }
+            chai_1.expect(actualArgs).equals(expectedArgs);
+        });
+    });
+    function makeHandlerFunction(paramNames) {
+        return eval(`(
+            function (${paramNames}) {
+                var args = [].slice.call(arguments);
+                return args.map(arg => arg.toString()).join(', ');
+            }
+        )`);
+    }
+});
 //# sourceMappingURL=normalize-handler.js.map
--- test/normalize-handler.ts
+++ test/normalize-handler.ts
@@ -1,18 +1,68 @@
-// 'use strict';
-// import {expect} from 'chai';
-// import normalizeHandler from '../src/normalize-handler';
-// 
-// 
-// describe('normalizeHandler', () => {
-// 
-// 
-//     it('works', () => {
-// 
-//         let h2 = normalizeHandler('/foo/{...path}/{name}.{ext}', (function (path, name, req, ext) {
-//             return null;
-//         }));
-// 
-//         let a = h2({ pathname: '/foo/bar/baz.html'});
-// 
-//     });
-// });
+'use strict';
+import {expect} from 'chai';
+import normalizeHandler from '../src/normalize-handler';
+
+
+describe('normalization of a handler function', () => {
+
+    let tests = [
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'rest',
+            handlerArgValues: 'foo/bar/baz.html'
+        },
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'req, rq, rest',
+            handlerArgValues: '[object Object], [object Object], foo/bar/baz.html'
+        },
+        {
+            pattern: '/api/…',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: '',
+            handlerArgValues: ''
+        },
+        {
+            pattern: '/api/{...rest}',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: '',
+            handlerArgValues: 'ERROR'
+        },
+        {
+            pattern: '/api/…',
+            pathname: '/api/foo/bar/baz.html',
+            handlerParamNames: 'rest',
+            handlerArgValues: 'ERROR'
+        },
+        {
+            pattern: '/foo/{...path}/{name}.{ext}',
+            pathname: '/foo/bar/baz.html',
+            handlerParamNames: 'path, name, req, ext',
+            handlerArgValues: 'bar, baz, [object Object], html'
+        }
+    ];
+
+    tests.forEach((test, i) => {
+        it(`${test.pattern} WITH (${test.handlerParamNames}) => {...}`, () => {
+            let handler = makeHandlerFunction(test.handlerParamNames);
+            let canonicalHandler = normalizeHandler(test.pattern, handler);
+            let request = { pathname: test.pathname };
+            let expectedArgs = test.handlerArgValues;
+            let actualArgs = 'ERROR';
+            try { actualArgs = <any> canonicalHandler(request); }
+            catch (ex) { }
+            expect(actualArgs).equals(expectedArgs);
+        });
+    });
+
+    function makeHandlerFunction(paramNames: string) {
+        return eval(`(
+            function (${paramNames}) {
+                var args = [].slice.call(arguments);
+                return args.map(arg => arg.toString()).join(', ');
+            }
+        )`);
+    }
+});
