6880ea345376030b645a9a4752da70e5e355fb67
Troy Gerwien <yortus@gmail.com>
2016-01-20 16:04:06 +0800

    remove dead code and files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ var hierarchize_patterns_1 = require('./hierarchize-patterns');
 exports.hierarchizePatterns = hierarchize_patterns_1.default;
 var intersect_patterns_1 = require('./intersect-patterns');
 exports.intersectPatterns = intersect_patterns_1.default;
-//export {default as normalizePattern} from './normalize-pattern';
 var pattern_1 = require('./pattern');
 exports.Pattern = pattern_1.default;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export {default as hierarchizePatterns} from './hierarchize-patterns';
 export {default as intersectPatterns} from './intersect-patterns';
-//export {default as normalizePattern} from './normalize-pattern';
 export {default as Pattern} from './pattern';
