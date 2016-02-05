678b53189bb55219922fd263c157f58379ee342b
yortus <yortus@gmail.com>
2016-02-05 17:10:54 +0800

    simplify decision tree

--- src/routers/make-decision-tree.js
+++ src/routers/make-decision-tree.js
@@ -1,43 +1,8 @@
 'use strict';
-// import * as assert from 'assert';
-// import {inspect} from 'util';
 var get_keys_deep_1 = require('./get-keys-deep');
 var pattern_1 = require('../patterns/pattern');
 // TODO: ...
 function makeDecisionTree(patternHierarchy) {
-    if (1 === 1)
-        return test(patternHierarchy);
-    // TODO: ...
-    var patternSignatures = get_keys_deep_1.default(patternHierarchy);
-    var quickMatchers = patternSignatures.reduce(function (map, sig) {
-        var p = new pattern_1.default(sig);
-        map[sig] = function quickMatch(address) { return p.match(address) !== null; };
-        return map;
-    }, {});
-    // TODO: ...
-    function getBestMatchingPatternSignature(address, currentBest, moreSpecificCandidates) {
-        // Construct a function that tries all the more specific candidates.
-        var candidateSignatures = Object.keys(moreSpecificCandidates);
-        var candidateMatchers = candidateSignatures.map(function (sig) { return quickMatchers[sig]; });
-        function tryMoreSpecificCandidates(address) {
-            for (var i = 0; i < candidateMatchers.length; ++i) {
-                if (candidateMatchers[i](address))
-                    return candidateSignatures[i];
-            }
-            return null;
-        }
-        var better = tryMoreSpecificCandidates(address);
-        if (!better)
-            return currentBest;
-        return getBestMatchingPatternSignature(address, better, moreSpecificCandidates[better]);
-    }
-    ;
-    // TODO: ...
-    return function (address) { return getBestMatchingPatternSignature(address, '…', patternHierarchy['…']); };
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = makeDecisionTree;
-function test(patternHierarchy) {
     // TODO: ...
     var patternSignatures = get_keys_deep_1.default(patternHierarchy);
     var patternMatchers = patternSignatures.reduce(function (map, sig) {
@@ -45,20 +10,6 @@ function test(patternHierarchy) {
         map[sig] = function (address) { return p.match(address) !== null; };
         return map;
     }, {});
-    // TODO: ...
-    //     function getIdsForPatterns(patterns: PatternHierarchy, prefix?: string): string[] {
-    //         let result: string[] = [];
-    //         Object.keys(patterns).forEach((sig, i) => {
-    //             let id = `${prefix || ''}_${i}`;
-    //             let rest = getIdsForPatterns(patterns[sig], id);
-    //             result.push(id);
-    //             result.push.apply(result, rest);
-    //         });
-    //         return result;
-    //     }
-    //     let ids = getIdsForPatterns(patternHierarchy);
-    //     console.log(ids);
-    // debugger;    
     // TODO: ...
     function getPrologLines(patterns, idPrefix) {
         var result = [];
@@ -74,34 +25,24 @@ function test(patternHierarchy) {
         return result;
     }
     var prolog = getPrologLines(patternHierarchy['…']);
-    function getBodyLines(thisPattern, childPatterns, idPrefix, linePrefix) {
-        linePrefix = linePrefix || '';
-        var result = [];
+    function getBodyLines(thisPattern, childPatterns, idPrefix) {
+        var lines = [];
         var signatures = Object.keys(childPatterns);
-        if (signatures.length > 0) {
+        var hasKids = signatures.length > 0;
+        if (hasKids) {
             signatures.forEach(function (sig, i) {
                 var id = (idPrefix || '') + "_" + i;
-                result.push("" + linePrefix + (i > 0 ? 'else ' : '') + "if (" + id + "(address)) {\n");
-                result.push.apply(result, getBodyLines(sig, childPatterns[sig], id, linePrefix + '    '));
-                result.push(linePrefix + "}\n");
+                lines.push.apply(lines, [(i > 0 ? 'else ' : '') + "if (" + id + "(address)) {"].concat(getBodyLines(sig, childPatterns[sig], id).map(function (line) { return '    ' + line; }), ["}"]));
             });
-            result.push(linePrefix + "else {\n");
-            result.push(linePrefix + "    return '" + thisPattern + "';\n");
-            result.push(linePrefix + "}\n");
         }
-        else {
-            // TODO: ...
-            result.push(linePrefix + "return '" + thisPattern + "';\n");
-        }
-        return result;
+        lines.push((hasKids ? 'else ' : '') + "return '" + thisPattern + "';");
+        return lines;
     }
-    var body = getBodyLines('…', patternHierarchy['…']);
-    var source = prolog.concat(body).map(function (line) { return '    ' + line; }).join('');
+    var body = getBodyLines('…', patternHierarchy['…']).map(function (line) { return line + '\n'; });
+    var source = prolog.concat(['\n'], body).map(function (line) { return '    ' + line; }).join('');
     var fn = eval("(function bestMatch(address) {\n" + source + "})");
     return fn;
 }
-// TODO: temp testing... non recursive optimised construction:
-"\n    let _ = patternSignatures.map(sig => patterns[sig].match);\n    let match0 =    _['\u2026'];\n    let match1 =        _['/foo']\n    let match2 =            _['/foo/bar']\n    let match3 =                _['/foo/bar/baz']\n    let match4 =            _['/foo/baz']\n    let match5 =        _['/bar']\n\n\n    if (match1(addr)) {\n        if (match2(addr)) {\n            if (match3(addr)) {\n                return '/foo/bar/baz';\n            }\n            else {\n                return '/foo/bar';\n            }\n        }\n        else if (match4(addr)) {\n            return '/foo/baz';\n        }\n        else {\n            return '/foo';\n        }\n    }\n    else if (match5(addr)) {\n        return '/bar';\n    }\n    else {\n        return '\u2026';\n    }\n";
-// TODO: temp testing... non recursive optimised construction:
-"\n    if (false) {\n    }\n    else if (quickMatchFoo(addr)) {\n        if (false) {\n        }\n        else if (quickMatchFooBar(addr)) {\n            if (false) {\n            }\n            else if (quickMatchFooBarBaz(addr)) {\n                if (false) {\n                }\n                else {\n                    return '/foo/bar/baz';\n                }\n            }\n            else {\n                return '/foo/bar';\n            }\n        }\n        else if (quickMatchFooBaz(addr)) {\n            if (false) {\n            }\n            else {\n                return '/foo/baz';\n            }\n        }\n        else {\n            return '/foo';\n        }\n    }\n    else if (quickMatchBar(addr)) {\n        if (false) {\n        }\n        else {\n            return '/bar';\n        }\n    }\n    else {\n        return '\u2026';\n    }\n";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = makeDecisionTree;
 //# sourceMappingURL=make-decision-tree.js.map
--- src/routers/make-decision-tree.ts
+++ src/routers/make-decision-tree.ts
@@ -1,13 +1,7 @@
 'use strict';
-// import * as assert from 'assert';
-// import {inspect} from 'util';
 import getKeysDeep from './get-keys-deep';
 import {PatternHierarchy} from '../patterns/hierarchize-patterns';
 import Pattern from '../patterns/pattern';
-//import Request from '../request';
-//import Response from '../response';
-// import Rule from '../rules/rule';
-// import walkPatternHierarchy from './walk-pattern-hierarchy';
 
 
 
@@ -22,48 +16,6 @@ type GetBestMatchingPatternSignature = (address: string) => string;
 // TODO: ...
 export default function makeDecisionTree(patternHierarchy: PatternHierarchy): GetBestMatchingPatternSignature {
 
-    if (1===1) return test(patternHierarchy);
-
-
-    // TODO: ...
-    let patternSignatures = getKeysDeep(patternHierarchy);
-
-    //TODO: ...
-    type QuickMatch = (address: string) => boolean;
-    let quickMatchers = patternSignatures.reduce((map, sig) => {
-        let p = new Pattern(sig);
-        map[sig] = function quickMatch(address: string) { return p.match(address) !== null; }
-        return map;
-    }, <{[pattern: string]: QuickMatch}>{});
-
-    // TODO: ...
-    function getBestMatchingPatternSignature(address: string, currentBest: string, moreSpecificCandidates: PatternHierarchy) {
-
-        // Construct a function that tries all the more specific candidates.
-        let candidateSignatures = Object.keys(moreSpecificCandidates);
-        let candidateMatchers = candidateSignatures.map(sig => quickMatchers[sig]);
-        function tryMoreSpecificCandidates(address: string) {
-            for (let i = 0; i < candidateMatchers.length; ++i) {
-                if (candidateMatchers[i](address)) return candidateSignatures[i];
-            }
-            return null;
-        }
-
-        let better = tryMoreSpecificCandidates(address);
-        if (!better) return currentBest;
-        return getBestMatchingPatternSignature(address, better, moreSpecificCandidates[better]);
-    };
-
-    // TODO: ...
-    return address => getBestMatchingPatternSignature(address, '…', patternHierarchy['…']);
-}
-
-
-
-
-
-function test(patternHierarchy: PatternHierarchy) {
-    
     // TODO: ...
     let patternSignatures = getKeysDeep(patternHierarchy);
 
@@ -74,22 +26,6 @@ function test(patternHierarchy: PatternHierarchy) {
         map[sig] = (address: string) => p.match(address) !== null;
         return map;
     }, <{[pattern: string]: QuickMatch}>{});
-
-    // TODO: ...
-//     function getIdsForPatterns(patterns: PatternHierarchy, prefix?: string): string[] {
-//         let result: string[] = [];
-//         Object.keys(patterns).forEach((sig, i) => {
-//             let id = `${prefix || ''}_${i}`;
-//             let rest = getIdsForPatterns(patterns[sig], id);
-//             result.push(id);
-//             result.push.apply(result, rest);
-//         });
-//         return result;
-//     }
-//     let ids = getIdsForPatterns(patternHierarchy);
-//     console.log(ids);
-// debugger;    
-
 
     // TODO: ...
     function getPrologLines(patterns: PatternHierarchy, idPrefix?: string): string[] {
@@ -107,119 +43,34 @@ function test(patternHierarchy: PatternHierarchy) {
     let prolog = getPrologLines(patternHierarchy['…']);
 
 
-    function getBodyLines(thisPattern: string, childPatterns: PatternHierarchy, idPrefix?: string, linePrefix?: string): string[] {
-        linePrefix = linePrefix || '';
-        let result: string[] = [];
+    function getBodyLines(thisPattern: string, childPatterns: PatternHierarchy, idPrefix?: string): string[] {
+        let lines: string[] = [];
         let signatures = Object.keys(childPatterns);
-        if (signatures.length > 0) {
+        let hasKids = signatures.length > 0;
+
+        if (hasKids) {
             signatures.forEach((sig, i) => {
                 let id = `${idPrefix || ''}_${i}`;
-                result.push(`${linePrefix}${i > 0 ? 'else ' : ''}if (${id}(address)) {\n`);
-                result.push.apply(result, getBodyLines(sig, childPatterns[sig], id, linePrefix + '    '));
-                result.push(`${linePrefix}}\n`);
+                lines.push(
+                    `${i > 0 ? 'else ' : ''}if (${id}(address)) {`,
+                    ...getBodyLines(sig, childPatterns[sig], id).map(line => '    ' + line),
+                    `}`
+                );
             });
-            result.push(`${linePrefix}else {\n`);
-            result.push(`${linePrefix}    return '${thisPattern}';\n`);
-            result.push(`${linePrefix}}\n`);
         }
-        else {
-            // TODO: ...
-            result.push(`${linePrefix}return '${thisPattern}';\n`);
-        }
-        return result;
+
+        lines.push(`${hasKids ? 'else ' : ''}return '${thisPattern}';`);
+
+
+        return lines;
     }
-    let body = getBodyLines('…', patternHierarchy['…']);
+    let body = getBodyLines('…', patternHierarchy['…']).map(line => line + '\n');
 
 
-    let source = prolog.concat(body).map(line => '    ' + line).join('');
+    let source = prolog.concat(['\n'], body).map(line => '    ' + line).join('');
     let fn = eval(`(function bestMatch(address) {\n${source}})`);
 
     return fn;
+
+
 }
-
-
-
-
-
-// TODO: temp testing... non recursive optimised construction:
-`
-    let _ = patternSignatures.map(sig => patterns[sig].match);
-    let match0 =    _['…'];
-    let match1 =        _['/foo']
-    let match2 =            _['/foo/bar']
-    let match3 =                _['/foo/bar/baz']
-    let match4 =            _['/foo/baz']
-    let match5 =        _['/bar']
-
-
-    if (match1(addr)) {
-        if (match2(addr)) {
-            if (match3(addr)) {
-                return '/foo/bar/baz';
-            }
-            else {
-                return '/foo/bar';
-            }
-        }
-        else if (match4(addr)) {
-            return '/foo/baz';
-        }
-        else {
-            return '/foo';
-        }
-    }
-    else if (match5(addr)) {
-        return '/bar';
-    }
-    else {
-        return '…';
-    }
-`;
-
-
-
-
-
-// TODO: temp testing... non recursive optimised construction:
-`
-    if (false) {
-    }
-    else if (quickMatchFoo(addr)) {
-        if (false) {
-        }
-        else if (quickMatchFooBar(addr)) {
-            if (false) {
-            }
-            else if (quickMatchFooBarBaz(addr)) {
-                if (false) {
-                }
-                else {
-                    return '/foo/bar/baz';
-                }
-            }
-            else {
-                return '/foo/bar';
-            }
-        }
-        else if (quickMatchFooBaz(addr)) {
-            if (false) {
-            }
-            else {
-                return '/foo/baz';
-            }
-        }
-        else {
-            return '/foo';
-        }
-    }
-    else if (quickMatchBar(addr)) {
-        if (false) {
-        }
-        else {
-            return '/bar';
-        }
-    }
-    else {
-        return '…';
-    }
-`;
