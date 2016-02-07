d79d3bced011121d5f961c478fd3bc811c943014
yortus <yortus@gmail.com>
2016-02-07 17:42:42 +0800

    temp. disable pattern-related tests

--- test/patterns/hierarchize-patterns.js
+++ test/patterns/hierarchize-patterns.js
@@ -1,152 +1,159 @@
-'use strict';
-var chai_1 = require('chai');
-var hierarchize_patterns_1 = require('../../src/patterns/hierarchize-patterns');
-var pattern_1 = require('../../src/patterns/pattern');
-describe('Hierarchizing a set of patterns', function () {
-    var tests = [
-        {
-            // ======================================== 1. ========================================
-            name: 'simple tree',
-            patterns: [
-                '/**',
-                '/foo/*',
-                '/bar/*',
-                '/foo/bar'
-            ],
-            hierarchy: {
-                "…": {
-                    "/…": {
-                        "/foo/*": {
-                            "/foo/bar": {}
-                        },
-                        "/bar/*": {}
-                    }
-                }
-            }
-        },
-        {
-            // ======================================== 2. ========================================
-            name: 'impossible intersections',
-            patterns: [
-                '**',
-                'a*',
-                '*a',
-            ],
-            hierarchy: 'ERROR: Intersection of *a and a* cannot be expressed as a single pattern...'
-        },
-        {
-            // ======================================== 3. ========================================
-            name: 'complex DAG',
-            patterns: [
-                'a*',
-                '*m*',
-                '*z',
-                '**',
-                '/bar',
-                '/*',
-                '/foo',
-                '/foo/*.html',
-                '/…o…o….html',
-                '/**o**o**',
-                '/bar',
-                'a*',
-                '/a/*',
-                '/*/b',
-                '/*z/b',
-            ],
-            hierarchy: {
-                "…": {
-                    "a*": {
-                        "a*m*": {
-                            "a*m*z": {}
-                        },
-                        "a*z": {
-                            "a*m*z": {}
-                        }
-                    },
-                    "*m*": {
-                        "a*m*": {
-                            "a*m*z": {}
-                        },
-                        "*m*z": {
-                            "a*m*z": {}
-                        }
-                    },
-                    "*z": {
-                        "a*z": {
-                            "a*m*z": {}
-                        },
-                        "*m*z": {
-                            "a*m*z": {}
-                        }
-                    },
-                    "/*": {
-                        "/bar": {},
-                        "/*o*o*": {
-                            "/foo": {},
-                            "/*o*o*.html": {}
-                        }
-                    },
-                    "/…o…o…": {
-                        "/*o*o*": {
-                            "/foo": {},
-                            "/*o*o*.html": {}
-                        },
-                        "/…o…o….html": {
-                            "/*o*o*.html": {},
-                            "/foo/*.html": {},
-                            "/a/*o*o*.html": {}
-                        },
-                        "/a/*o*o*": {
-                            "/a/*o*o*.html": {}
-                        },
-                        "/*o*o*/b": {
-                            "/*o*o*z/b": {}
-                        }
-                    },
-                    "/a/*": {
-                        "/a/*o*o*": {
-                            "/a/*o*o*.html": {}
-                        },
-                        "/a/b": {}
-                    },
-                    "/*/b": {
-                        "/*o*o*/b": {
-                            "/*o*o*z/b": {}
-                        },
-                        "/a/b": {},
-                        "/*z/b": {
-                            "/*o*o*z/b": {}
-                        }
-                    },
-                    "/*z/b": {
-                        "/*o*o*z/b": {}
-                    }
-                }
-            }
-        }
-    ];
-    tests.forEach(function (test) {
-        it(test.name, function () {
-            var patterns = test.patterns.map(function (ps) { return new pattern_1.default(ps); });
-            var expected = test.hierarchy;
-            var actual;
-            try {
-                actual = mapToObj(hierarchize_patterns_1.default(patterns));
-            }
-            catch (ex) {
-                actual = 'ERROR: ' + ex.message;
-                if (typeof expected === 'string' && expected.slice(-3) === '...') {
-                    actual = actual.slice(0, expected.length - 3) + '...';
-                }
-            }
-            chai_1.expect(actual).deep.equal(expected);
-        });
-    });
-});
-/** Helper function that converts a Graph<Pattern> to a simple nested object with pattern sources for keys */
-function mapToObj(map) {
-    var patterns = Array.from(map.keys());
-    return patterns.reduce(function (obj, pat) { return (obj[pat.source] = mapToObj(map.get(pat)), obj); }, {});
-}
+// 'use strict';
+// import {expect} from 'chai';
+// import hierarchizePatterns from '../../src/patterns/hierarchize-patterns';
+// import {Graph} from '../../src/util';
+// import Pattern from '../../src/patterns/pattern';
+// 
+// 
+// describe('Hierarchizing a set of patterns', () => {
+// 
+//     let tests = [
+//         {
+//             // ======================================== 1. ========================================
+//             name: 'simple tree',
+//             patterns: [
+//                 '/**',
+//                 '/foo/*',
+//                 '/bar/*',
+//                 '/foo/bar'
+//             ],
+//             hierarchy: {
+//                 "…": {
+//                     "/…": {
+//                         "/foo/*": {
+//                             "/foo/bar": {}
+//                         },
+//                         "/bar/*": {}
+//                     }
+//                 }
+//             }
+//         },
+//         {
+//             // ======================================== 2. ========================================
+//             name: 'impossible intersections',
+//             patterns: [
+//                 '**',
+//                 'a*',
+//                 '*a',
+//             ],
+//             hierarchy: 'ERROR: Intersection of *a and a* cannot be expressed as a single pattern...'
+//         },
+//         {
+//             // ======================================== 3. ========================================
+//             name: 'complex DAG',
+//             patterns: [
+//                 'a*',
+//                 '*m*',
+//                 '*z',
+//                 '**',
+//                 '/bar',
+//                 '/*',
+//                 '/foo',
+//                 '/foo/*.html',
+//                 '/…o…o….html',
+//                 '/**o**o**',
+//                 '/bar',
+//                 'a*',
+//                 '/a/*',
+//                 '/*/b',
+//                 '/*z/b',
+//             ],
+//             hierarchy: {
+//                 "…": {
+//                     "a*": {
+//                         "a*m*": {
+//                             "a*m*z": {}
+//                         },
+//                         "a*z": {
+//                             "a*m*z": {}
+//                         }
+//                     },
+//                     "*m*": {
+//                         "a*m*": {
+//                             "a*m*z": {}
+//                         },
+//                         "*m*z": {
+//                             "a*m*z": {}
+//                         }
+//                     },
+//                     "*z": {
+//                         "a*z": {
+//                             "a*m*z": {}
+//                         },
+//                         "*m*z": {
+//                             "a*m*z": {}
+//                         }
+//                     },
+//                     "/*": {
+//                         "/bar": {},
+//                         "/*o*o*": {
+//                             "/foo": {},
+//                             "/*o*o*.html": {}
+//                         }
+//                     },
+//                     "/…o…o…": {
+//                         "/*o*o*": {
+//                             "/foo": {},
+//                             "/*o*o*.html": {}
+//                         },
+//                         "/…o…o….html": {
+//                             "/*o*o*.html": {},
+//                             "/foo/*.html": {},
+//                             "/a/*o*o*.html": {}
+//                         },
+//                         "/a/*o*o*": {
+//                             "/a/*o*o*.html": {}
+//                         },
+//                         "/*o*o*/b": {
+//                             "/*o*o*z/b": {}
+//                         }
+//                     },
+//                     "/a/*": {
+//                         "/a/*o*o*": {
+//                             "/a/*o*o*.html": {}
+//                         },
+//                         "/a/b": {}
+//                     },
+//                     "/*/b": {
+//                         "/*o*o*/b": {
+//                             "/*o*o*z/b": {}
+//                         },
+//                         "/a/b": {},
+//                         "/*z/b": {
+//                             "/*o*o*z/b": {}
+//                         }
+//                     },
+//                     "/*z/b": {
+//                         "/*o*o*z/b": {}
+//                     }
+//                 }
+//             }
+//         }
+//     ];
+// 
+//     tests.forEach(test => {
+//         it(test.name, () => {
+//             let patterns = test.patterns.map(ps => new Pattern(ps));
+//             let expected: any = test.hierarchy;
+//             let actual: any;
+//             try {
+//                 actual = mapToObj(hierarchizePatterns(patterns));
+//             }
+//             catch (ex) {
+//                 actual = 'ERROR: ' + ex.message;
+//                 if (typeof expected === 'string' && expected.slice(-3) === '...') {
+//                     actual = actual.slice(0, expected.length - 3) + '...';
+//                 }
+//             }
+//             expect(actual).deep.equal(expected);
+//         });
+//     });
+// });
+// 
+// 
+// /** Helper function that converts a Graph<Pattern> to a simple nested object with pattern sources for keys */
+// function mapToObj(map: Graph<any>) {
+//     let patterns = Array.from(map.keys());
+//     return patterns.reduce((obj, pat) => (obj[pat.source] = mapToObj(map.get(pat)), obj), {});
+// }
 //# sourceMappingURL=hierarchize-patterns.js.map
--- test/patterns/pattern.js
+++ test/patterns/pattern.js
@@ -1,82 +1,90 @@
-'use strict';
-var chai_1 = require('chai');
-var pattern_1 = require('../../src/patterns/pattern');
-describe('Constructing a Pattern instance', function () {
-    var tests = [
-        '/api/foo ==> /api/foo WITH []',
-        '/api/foo/BAR ==> /api/foo/BAR WITH []',
-        '/api/foo… ==> /api/foo… WITH []',
-        '/api/foo** ==> /api/foo… WITH []',
-        '/api/foo/** ==> /api/foo/… WITH []',
-        '/api/foo/{...rest} ==> /api/foo/… WITH ["rest"]',
-        '/API/f* ==> /API/f* WITH []',
-        '/api/{foO}O ==> /api/*O WITH ["foO"]',
-        '/…/{name}.{ext} ==> /…/*.* WITH ["name", "ext"]',
-        '/**/{name}.{ext} ==> /…/*.* WITH ["name", "ext"]',
-        '/{...aPath}/{name}.{ext} ==> /…/*.* WITH ["aPath", "name", "ext"]',
-        '/-/./- ==> /-/./-',
-        '/*** ==> ERROR',
-        '/*… ==> ERROR',
-        '/foo/{...rest}* ==> ERROR',
-        '/foo/{name}{ext} ==> ERROR',
-        '/$foo ==> ERROR',
-        '/bar/? ==> ERROR',
-        '{} ==> ERROR',
-        '{a...} ==> ERROR',
-        '{...} ==> ERROR',
-        '{..} ==> ERROR',
-        '{..a} ==> ERROR',
-        '{foo-bar} ==> ERROR',
-        '{"foo"} ==> ERROR',
-        '{ ==> ERROR',
-        '} ==> ERROR',
-        '{{} ==> ERROR',
-        '{}} ==> ERROR',
-        '{$} ==> * WITH ["$"]',
-        '{...__} ==> … WITH ["__"]',
-        '#comment ==> ',
-        '   #comment ==> ',
-        '# /a/b/c   fsdfsdf ==> ',
-        '/a/b#comment ==> /a/b',
-        '/**/{name}.js   #12 ==> /…/*.js WITH ["name"]',
-    ];
-    tests.forEach(function (test) {
-        it(test, function () {
-            var patternSource = test.split(' ==> ')[0];
-            var rhs = test.split(' ==> ')[1];
-            var expectedSignature = rhs.split(' WITH ')[0];
-            var expectedCaptureNames = eval(rhs.split(' WITH ')[1] || '[]');
-            var actualSignature = 'ERROR';
-            var actualCaptureNames = [];
-            try {
-                var pattern = new pattern_1.default(patternSource);
-                actualSignature = pattern.normalized.toString();
-                actualCaptureNames = pattern.captureNames;
-            }
-            catch (ex) { }
-            chai_1.expect(actualSignature).equals(expectedSignature);
-            chai_1.expect(actualCaptureNames).to.deep.equal(expectedCaptureNames);
-        });
-    });
-});
-describe('Comparing patterns with their normalized forms', function () {
-    var patterns = [
-        '/*/bar/{...baz}',
-        '/*/bar/…',
-        '/{n}/bar/**',
-        '/{__}/bar/{...baz}'
-    ];
-    patterns.forEach(function (a1) {
-        var p1 = new pattern_1.default(a1);
-        it("'" + a1 + "' vs normalised", function () {
-            chai_1.expect(a1 === p1.normalized.toString()).equals(p1 === p1.normalized);
-        });
-        patterns.forEach(function (a2) {
-            var p2 = new pattern_1.default(a2);
-            it("'" + a1 + "' vs '" + a2 + "'", function () {
-                chai_1.expect(p1.normalized).equals(p2.normalized);
-            });
-        });
-    });
-});
+// 'use strict';
+// import {expect} from 'chai';
+// import Pattern from '../../src/patterns/pattern';
+// 
+// 
+// describe('Constructing a Pattern instance', () => {
+// 
+//     let tests = [
+//         '/api/foo ==> /api/foo WITH []',
+//         '/api/foo/BAR ==> /api/foo/BAR WITH []',
+//         '/api/foo… ==> /api/foo… WITH []',
+//         '/api/foo** ==> /api/foo… WITH []',
+//         '/api/foo/** ==> /api/foo/… WITH []',
+//         '/api/foo/{...rest} ==> /api/foo/… WITH ["rest"]',
+//         '/API/f* ==> /API/f* WITH []',
+//         '/api/{foO}O ==> /api/*O WITH ["foO"]',
+//         '/…/{name}.{ext} ==> /…/*.* WITH ["name", "ext"]',
+//         '/**/{name}.{ext} ==> /…/*.* WITH ["name", "ext"]',
+//         '/{...aPath}/{name}.{ext} ==> /…/*.* WITH ["aPath", "name", "ext"]',
+//         '/-/./- ==> /-/./-',
+//         '/*** ==> ERROR',
+//         '/*… ==> ERROR',
+//         '/foo/{...rest}* ==> ERROR',
+//         '/foo/{name}{ext} ==> ERROR',
+//         '/$foo ==> ERROR',
+//         '/bar/? ==> ERROR',
+//         '{} ==> ERROR',
+//         '{a...} ==> ERROR',
+//         '{...} ==> ERROR',
+//         '{..} ==> ERROR',
+//         '{..a} ==> ERROR',
+//         '{foo-bar} ==> ERROR',
+//         '{"foo"} ==> ERROR',
+//         '{ ==> ERROR',
+//         '} ==> ERROR',
+//         '{{} ==> ERROR',
+//         '{}} ==> ERROR',
+//         '{$} ==> * WITH ["$"]',
+//         '{...__} ==> … WITH ["__"]',
+//         '#comment ==> ',
+//         '   #comment ==> ',
+//         '# /a/b/c   fsdfsdf ==> ',
+//         '/a/b#comment ==> /a/b',
+//         '/**/{name}.js   #12 ==> /…/*.js WITH ["name"]',
+//     ];
+// 
+//     tests.forEach(test => {
+//         it(test, () => {
+//             let patternSource = test.split(' ==> ')[0];
+//             let rhs = test.split(' ==> ')[1];
+//             let expectedSignature = rhs.split(' WITH ')[0];
+//             let expectedCaptureNames = eval(rhs.split(' WITH ')[1] || '[]');
+//             let actualSignature = 'ERROR';
+//             let actualCaptureNames = [];
+//             try {
+//                 let pattern = new Pattern(patternSource);
+//                 actualSignature = pattern.normalized.toString();
+//                 actualCaptureNames = pattern.captureNames;
+//             }
+//             catch (ex) { }
+//             expect(actualSignature).equals(expectedSignature);
+//             expect(actualCaptureNames).to.deep.equal(expectedCaptureNames);
+//         });
+//     });
+// });
+// 
+// 
+// describe('Comparing patterns with their normalized forms', () => {
+//     let patterns = [
+//         '/*/bar/{...baz}',
+//         '/*/bar/…',
+//         '/{n}/bar/**',
+//         '/{__}/bar/{...baz}'
+//     ];
+// 
+//     patterns.forEach(a1 => {
+//         let p1 = new Pattern(a1);
+//         it(`'${a1}' vs normalised`, () => {
+//             expect(a1 === p1.normalized.toString()).equals(p1 === p1.normalized);
+//         });
+// 
+//         patterns.forEach(a2 => {
+//             let p2 = new Pattern(a2);
+//             it(`'${a1}' vs '${a2}'`, () => {
+//                 expect(p1.normalized).equals(p2.normalized);
+//             });
+//         });
+//     });
+// });
 //# sourceMappingURL=pattern.js.map
