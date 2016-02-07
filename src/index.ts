c3db2bb644719efade6f1fb4a48dc1126f7057e4
yortus <yortus@gmail.com>
2016-02-07 13:57:07 +0800

    remove unused index file

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as hierarchizePatterns} from './patterns/hierarchize-patterns';
-export {default as intersectPatterns} from './patterns/intersect-patterns';
-export {default as Pattern} from './patterns/pattern';
-export {default as Rule} from './rules/rule';
