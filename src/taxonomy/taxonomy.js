dd5bbf870488659f02f59dc415d2278f40012586
yortus <yortus@gmail.com>
2016-02-18 21:28:32 +0800

    more taxonomy cleanup

--- src/taxonomy/taxonomy.js
+++ src/taxonomy/taxonomy.js
@@ -3,10 +3,40 @@ var insert_as_descendent_1 = require('./insert-as-descendent');
 var pattern_1 = require('../pattern');
 // TODO: review all docs below after data structure changes
 /**
- * A taxonomy is a directed acyclic graph whose nodes are patterns. The graph is arranged such that
+ * A taxonomy is a directed acyclic graph
+ *
+ * whose nodes are patterns. The graph is arranged such that
  * for any two nodes P and Q, P is an ancestor of Q iff if the addresses matched by P are a proper
  * superset of the addresses matched by Q.
  * TODO: more...
+ *
+ * Structurally, each Taxonomy instance may be considered both the root of a whole graph of nodes,
+ * as well as an individual node within a wider graph.
+ *
+ */
+/**
+ * Arranges the given list of patterns into a directed acyclic graph (DAG), according to their set
+ * relationships (recall that each pattern represents a set of addresses). The arrangement is akin
+ * to a Venn diagram. The 'outermost' pattern is always the the universal set ('…'), regardless of
+ * whether `patterns` contains a '…'. For any two patterns P and Q, if Q is a proper subset of P,
+ * then Q will be a descendent of P in the DAG. Overlapping patterns (i.e., patterns whose
+ * intersection is non-empty and where neither is a subset of the other) are represented as
+ * siblings in the taxonomy.
+ * NB1: For overlapping patterns, an additional pattern representing their
+ * intersection is synthesized and added as a descendent of both patterns. As such, the DAG may
+ * contain patterns that do not correspond to any input pattern.
+ * NB2: All patterns in the returned graph are guaranteed to be normalized. As such, some of the
+ * input `patterns` may not appear in the output graph, but their normalized equivalents will.
+ *
+ * For example, the patterns ['/foo', '/bar', '/f*', '/*o'] result in the DAG:
+ * …
+ * |-- /bar
+ * |-- /f*
+ * |   |-- /f*o
+ * |       |-- /foo
+ * |-- /*o
+ *     |-- /f*o
+ *         |-- /foo
  */
 var Taxonomy = (function () {
     /**
@@ -23,9 +53,23 @@ var Taxonomy = (function () {
     // TODO: doc better...
     /**
      * Generates a new Taxonomy instance based on the given set of patterns.
+     * @param {Pattern[]} patterns - the list of patterns that make up nodes in the DAG.
+     * @returns {Graph<Pattern>} A map object, whose keys are patterns and whose values
+     *        are more maps. The top-level map always contains the single key '…' All
+     *        patterns in the returned graph are normalized.
      */
     Taxonomy.from = function (patterns) {
-        var taxonomy = makeTaxonomy(patterns, function (pattern) { return new Taxonomy(pattern); });
+        // Create the nodeFor() function to return the graph node corresponding to a given
+        // pattern, creating it on demand if it doesn't already exist. This function ensures
+        // that every request for the same pattern gets the same singleton node.
+        var allNodes = new Map();
+        var nodeFor = function (pattern) {
+            if (!allNodes.has(pattern))
+                allNodes.set(pattern, new Taxonomy(pattern));
+            return allNodes.get(pattern);
+        };
+        // TODO: delegate...
+        var taxonomy = makeTaxonomy(patterns, nodeFor);
         // TODO: should freeze whole graph, not just root node...
         Object.freeze(taxonomy.generalizations);
         Object.freeze(taxonomy.specializations);
@@ -63,6 +107,18 @@ var Taxonomy = (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Taxonomy;
 // TODO: doc...
+function makeTaxonomy(patterns, nodeFor) {
+    // Insert each of the given patterns (except '…' and '∅') into a DAG rooted at '…'.
+    // The rest of the algorithm assumes only normalized patterns, which we obtain here.
+    patterns
+        .map(function (pat) { return pat.normalized; })
+        .filter(function (p) { return p !== pattern_1.default.UNIVERSAL && p !== pattern_1.default.EMPTY; })
+        .forEach(function (p) { return insert_as_descendent_1.default(p, pattern_1.default.UNIVERSAL, nodeFor); });
+    // Return a new top-level node with the single key '…'.
+    var taxonomy = nodeFor(pattern_1.default.UNIVERSAL);
+    return taxonomy;
+}
+// TODO: doc...
 function getAllPatterns(taxonomy) {
     var allWithDups = (_a = [taxonomy.pattern]).concat.apply(_a, taxonomy.specializations.map(function (spec) { return spec.allPatterns; }));
     var resultSet = allWithDups.reduce(function (set, pat) { return set.add(pat); }, new Set());
@@ -75,55 +131,5 @@ function getAllPathsFromHere(taxonomy) {
     var allChildPaths = (_a = [[]]).concat.apply(_a, taxonomy.specializations.map(function (spec) { return spec.allPathsFromHere; }));
     return allChildPaths.map(function (childPath) { return [taxonomy.pattern].concat(childPath); });
     var _a;
-}
-/**
- * Arranges the given list of patterns into a directed acyclic graph (DAG), according to their set
- * relationships (recall that each pattern represents a set of addresses). The arrangement is akin
- * to a Venn diagram. The 'outermost' pattern is always the the universal set ('…'), regardless of
- * whether `patterns` contains a '…'. For any two patterns P and Q, if Q is a proper subset of P,
- * then Q will be a descendent of P in the DAG. Overlapping patterns (i.e., patterns whose
- * intersection is non-empty and where neither is a subset of the other) are represented as
- * siblings in the taxonomy. For overlapping patterns, an additional pattern representing their
- * intersection is synthesized and added as a descendent of both patterns.
- * NB: All patterns in the returned graph are guaranteed to be normalized. As such, some of the
- * input `patterns` may not appear in the output graph, but their normalized equivalents will.
- *
- * For example, the patterns ['/foo', '/bar', '/f*', '/*o'] result in the DAG:
- * …
- * |-- /bar
- * |-- /f*
- * |   |-- /f*o
- * |       |-- /foo
- * |-- /*o
- *     |-- /f*o
- *         |-- /foo
- *
- * @param {Pattern[]} patterns - the list of patterns that make up nodes in the DAG.
- * @returns {Graph<Pattern>} A map object, whose keys are patterns and whose values
- *        are more maps. The top-level map always contains the single key '…' All
- *        patterns in the returned graph are normalized.
- */
-function makeTaxonomy(patterns, factory) {
-    // The rest of the algorithm assumes only normalized patterns, which we obtain here.
-    var normalizedPatterns = patterns.map(function (pat) { return pat.normalized; });
-    // Create the nodeFor() function to return the graph node corresponding to a given
-    // pattern, creating it on demand if it doesn't already exist. This function ensures
-    // that every request for the same pattern gets the same singleton node.
-    var allNodes = new Map();
-    var nodeFor = function (pattern) {
-        var node = allNodes.get(pattern);
-        if (!node) {
-            node = factory(pattern);
-            allNodes.set(pattern, node);
-        }
-        return node;
-    };
-    // Insert each of the given patterns (except '…' and '∅') into a DAG rooted at '…'.
-    normalizedPatterns
-        .filter(function (p) { return p !== pattern_1.default.UNIVERSAL && p !== pattern_1.default.EMPTY; })
-        .forEach(function (p) { return insert_as_descendent_1.default(p, pattern_1.default.UNIVERSAL, nodeFor); });
-    // Return a new top-level node with the single key '…'.
-    var taxonomy = nodeFor(pattern_1.default.UNIVERSAL);
-    return taxonomy;
 }
 //# sourceMappingURL=taxonomy.js.map
--- src/taxonomy/taxonomy.ts
+++ src/taxonomy/taxonomy.ts
@@ -8,10 +8,45 @@ import Pattern from '../pattern';
 
 
 /**
- * A taxonomy is a directed acyclic graph whose nodes are patterns. The graph is arranged such that
+ * A taxonomy is a directed acyclic graph
+ * 
+ * whose nodes are patterns. The graph is arranged such that
  * for any two nodes P and Q, P is an ancestor of Q iff if the addresses matched by P are a proper
  * superset of the addresses matched by Q.
  * TODO: more...
+ *
+ * Structurally, each Taxonomy instance may be considered both the root of a whole graph of nodes,
+ * as well as an individual node within a wider graph.
+ *
+ */
+
+
+
+
+
+/**
+ * Arranges the given list of patterns into a directed acyclic graph (DAG), according to their set
+ * relationships (recall that each pattern represents a set of addresses). The arrangement is akin
+ * to a Venn diagram. The 'outermost' pattern is always the the universal set ('…'), regardless of
+ * whether `patterns` contains a '…'. For any two patterns P and Q, if Q is a proper subset of P,
+ * then Q will be a descendent of P in the DAG. Overlapping patterns (i.e., patterns whose
+ * intersection is non-empty and where neither is a subset of the other) are represented as
+ * siblings in the taxonomy.
+ * NB1: For overlapping patterns, an additional pattern representing their
+ * intersection is synthesized and added as a descendent of both patterns. As such, the DAG may
+ * contain patterns that do not correspond to any input pattern.
+ * NB2: All patterns in the returned graph are guaranteed to be normalized. As such, some of the
+ * input `patterns` may not appear in the output graph, but their normalized equivalents will.
+ * 
+ * For example, the patterns ['/foo', '/bar', '/f*', '/*o'] result in the DAG:
+ * …
+ * |-- /bar
+ * |-- /f*
+ * |   |-- /f*o
+ * |       |-- /foo
+ * |-- /*o
+ *     |-- /f*o
+ *         |-- /foo
  */
 export default class Taxonomy {
 
@@ -28,9 +63,24 @@ export default class Taxonomy {
     // TODO: doc better...
     /**
      * Generates a new Taxonomy instance based on the given set of patterns.
+     * @param {Pattern[]} patterns - the list of patterns that make up nodes in the DAG.
+     * @returns {Graph<Pattern>} A map object, whose keys are patterns and whose values
+     *        are more maps. The top-level map always contains the single key '…' All
+     *        patterns in the returned graph are normalized.
      */
     static from(patterns: Pattern[]) {
-        let taxonomy = makeTaxonomy(patterns, pattern => new Taxonomy(pattern));
+
+        // Create the nodeFor() function to return the graph node corresponding to a given
+        // pattern, creating it on demand if it doesn't already exist. This function ensures
+        // that every request for the same pattern gets the same singleton node.
+        let allNodes = new Map<Pattern, Taxonomy>();
+        let nodeFor = (pattern: Pattern) => {
+            if (!allNodes.has(pattern)) allNodes.set(pattern, new Taxonomy(pattern));
+            return allNodes.get(pattern);
+        }
+
+        // TODO: delegate...
+        let taxonomy = makeTaxonomy(patterns, nodeFor);
 
         // TODO: should freeze whole graph, not just root node...
         Object.freeze(taxonomy.generalizations);
@@ -88,6 +138,25 @@ export default class Taxonomy {
 
 
 // TODO: doc...
+function makeTaxonomy(patterns: Pattern[], nodeFor: (pattern: Pattern) => Taxonomy): Taxonomy {
+
+    // Insert each of the given patterns (except '…' and '∅') into a DAG rooted at '…'.
+    // The rest of the algorithm assumes only normalized patterns, which we obtain here.
+    patterns
+        .map(pat => pat.normalized)
+        .filter(p => p !== Pattern.UNIVERSAL && p !== Pattern.EMPTY)
+        .forEach(p => insertAsDescendent(p, Pattern.UNIVERSAL, nodeFor));
+
+    // Return a new top-level node with the single key '…'.
+    let taxonomy = nodeFor(Pattern.UNIVERSAL);
+    return taxonomy;
+}
+
+
+
+
+
+// TODO: doc...
 function getAllPatterns(taxonomy: Taxonomy): Pattern[] {
     let allWithDups = [taxonomy.pattern].concat(...taxonomy.specializations.map(spec => spec.allPatterns));
     let resultSet = allWithDups.reduce((set, pat) => set.add(pat), new Set<Pattern>());
@@ -103,78 +172,4 @@ function getAllPathsFromHere(taxonomy: Taxonomy): Pattern[][] {
     // TODO: test/review/cleanup...
     let allChildPaths: Pattern[][] = [[]].concat(...taxonomy.specializations.map(spec => spec.allPathsFromHere));
     return allChildPaths.map(childPath => [taxonomy.pattern].concat(childPath));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * Arranges the given list of patterns into a directed acyclic graph (DAG), according to their set
- * relationships (recall that each pattern represents a set of addresses). The arrangement is akin
- * to a Venn diagram. The 'outermost' pattern is always the the universal set ('…'), regardless of
- * whether `patterns` contains a '…'. For any two patterns P and Q, if Q is a proper subset of P,
- * then Q will be a descendent of P in the DAG. Overlapping patterns (i.e., patterns whose
- * intersection is non-empty and where neither is a subset of the other) are represented as
- * siblings in the taxonomy. For overlapping patterns, an additional pattern representing their
- * intersection is synthesized and added as a descendent of both patterns.
- * NB: All patterns in the returned graph are guaranteed to be normalized. As such, some of the
- * input `patterns` may not appear in the output graph, but their normalized equivalents will.
- * 
- * For example, the patterns ['/foo', '/bar', '/f*', '/*o'] result in the DAG:
- * …
- * |-- /bar
- * |-- /f*
- * |   |-- /f*o
- * |       |-- /foo
- * |-- /*o
- *     |-- /f*o
- *         |-- /foo
- * 
- * @param {Pattern[]} patterns - the list of patterns that make up nodes in the DAG.
- * @returns {Graph<Pattern>} A map object, whose keys are patterns and whose values
- *        are more maps. The top-level map always contains the single key '…' All
- *        patterns in the returned graph are normalized.
- */
-function makeTaxonomy(patterns: Pattern[], factory: (pattern: Pattern) => Taxonomy): Taxonomy {
-
-    // The rest of the algorithm assumes only normalized patterns, which we obtain here.
-    let normalizedPatterns = patterns.map(pat => pat.normalized);
-
-    // Create the nodeFor() function to return the graph node corresponding to a given
-    // pattern, creating it on demand if it doesn't already exist. This function ensures
-    // that every request for the same pattern gets the same singleton node.
-    let allNodes = new Map<Pattern, Taxonomy>();
-    let nodeFor = (pattern: Pattern) => {
-        let node = allNodes.get(pattern);
-        if (!node) {
-            node = factory(pattern);
-            allNodes.set(pattern, node);
-        }
-        return node;
-    }
-
-    // Insert each of the given patterns (except '…' and '∅') into a DAG rooted at '…'.
-    normalizedPatterns
-        .filter(p => p !== Pattern.UNIVERSAL && p !== Pattern.EMPTY)
-        .forEach(p => insertAsDescendent(p, Pattern.UNIVERSAL, nodeFor));
-
-    // Return a new top-level node with the single key '…'.
-    let taxonomy = nodeFor(Pattern.UNIVERSAL);
-    return taxonomy;
 }
