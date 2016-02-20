2840660363bb0a6e5febb5cd2c1a986e981a151c
yortus <yortus@gmail.com>
2016-02-20 14:16:06 +0800

    add docs/notes (WIP)

--- src/routing/make-route-handler.ts
+++ src/routing/make-route-handler.ts
@@ -43,3 +43,71 @@ export default function makeRouteHandler(route: Route): Handler {
 
 // TODO: ...
 const nullHandler: Handler = (address, request) => null;
+
+
+//TODO:
+// all handlers: pass in 'address' sneakily as 'this' via Function#call (still very fast!)
+
+
+// TODO: temp testing...
+`
+function makeHandler(rules: Rule[]) {
+    say rules = {
+        0: <Root>Partial,
+        1: Partial,
+        2: Decorator,
+        3: Partial,
+        4: Partial,
+        5: <Leaf>Partial
+    }
+
+
+    let handle3_4_5 = function (address, request) {
+        var response;
+        if ((response = handler5(address, request)) !== null) return response;
+        if ((response = handler4(address, request)) !== null) return response;
+        if ((response = handler3(address, request)) !== null) return response;
+        return null;
+    }
+
+    let handle2 = function (address, request) {
+
+        // NB: closes over address... therefore need new function/closure for EVERY request!!! how to avoid??? Function#bind?
+        let downstream = function (req) {
+            req = arguments.length > 0 ? req : request;
+            return handle3_4_5(address, req);
+        };
+
+        var response;
+        response = handler2(address, request, handle3_4_5);
+        return response;
+    }
+
+    let handle0_1_2 = function (address, request) {
+        var response;
+        if ((response = handler2(address, request)) !== null) return response;
+        if ((response = handler1(address, request)) !== null) return response;
+        if ((response = handler0(address, request)) !== null) return response;
+        return null;
+    }
+
+    let finalHandler = (address, request) {
+        return handle0_1_2(address, request);
+    }
+
+    
+
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+`
