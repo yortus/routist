3510b62fcfe3db1aa9088df6c5ef10466179afaa
yortus <yortus@gmail.com>
2016-02-21 15:27:09 +0800

    makeDispatcher cleanup

--- src/routing/make-dispatcher.js
+++ src/routing/make-dispatcher.js
@@ -10,31 +10,6 @@ function makeDispatcher(taxonomy, targetMap) {
     var patterns = taxonomy.allNodes.map(function (node) { return node.pattern; });
     var targets = patterns.map(function (pat) { return targetMap.get(pat); });
     // TODO: doc...
-    function getBodyLines(specializations, fallback, nestDepth) {
-        var indent = ' '.repeat(nestDepth * 4);
-        var firstLines = specializations.reduce(function (lines, node, i) {
-            var nextLevel = node.specializations;
-            var isLeaf = nextLevel.length === 0;
-            var id = make_pattern_identifier_1.default(node.pattern);
-            var condition = "" + indent + (i > 0 ? 'else ' : '') + "if (matches_" + id + "(address)) ";
-            // let condition = `${indent}${i > 0 ? 'else ' : ''}if (matches_${id}(address)) `;
-            // let consequent = isLeaf ? `return _${id};\n` : `{\n${getBody(nextLevel, spec.pattern, nestDepth + 1)}${indent}}\n`; // TODO: shorten to <120
-            // return condition + consequent;
-            if (isLeaf) {
-                var result_1 = lines.concat(condition + "return _" + id + ";");
-                return result_1;
-            }
-            var result = lines.concat([
-                (condition + "{")
-            ].concat(getBodyLines(nextLevel, node.pattern, nestDepth + 1), [
-                (indent + "}")
-            ]));
-            return result;
-        }, []);
-        var lastLine = indent + "return _" + make_pattern_identifier_1.default(fallback) + ";";
-        return firstLines.concat(lastLine);
-    }
-    // TODO: doc...
     var lines = patterns.map(function (pat, i) { return ("let matches_" + make_pattern_identifier_1.default(pat) + " = patterns[" + i + "].match;"); }).concat(patterns.map(function (pat, i) { return ("let _" + make_pattern_identifier_1.default(pat) + " = targets[" + i + "];"); }), [
         '',
         'return function dispatch(address) {'
@@ -51,4 +26,23 @@ function makeDispatcher(taxonomy, targetMap) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = makeDispatcher;
+// TODO: doc...
+function getBodyLines(specializations, fallback, nestDepth) {
+    var indent = ' '.repeat(nestDepth * 4);
+    var lines = [];
+    specializations.forEach(function (node, i) {
+        var id = make_pattern_identifier_1.default(node.pattern);
+        var condition = "" + indent + (i > 0 ? 'else ' : '') + "if (matches_" + id + "(address)) ";
+        var nextLevel = node.specializations;
+        if (nextLevel.length === 0)
+            return lines.push(condition + "return _" + id + ";");
+        lines.push.apply(lines, [
+            (condition + "{")
+        ].concat(getBodyLines(nextLevel, node.pattern, nestDepth + 1), [
+            (indent + "}")
+        ]));
+    });
+    lines.push(indent + "return _" + make_pattern_identifier_1.default(fallback) + ";");
+    return lines;
+}
 //# sourceMappingURL=make-dispatcher.js.map
--- src/routing/make-dispatcher.ts
+++ src/routing/make-dispatcher.ts
@@ -18,37 +18,6 @@ export default function makeDispatcher<T>(taxonomy: Taxonomy, targetMap: Map<Pat
     let targets = patterns.map(pat => targetMap.get(pat));
 
     // TODO: doc...
-    function getBodyLines(specializations: TaxonomyNode[], fallback: Pattern, nestDepth: number) {
-        let indent = ' '.repeat(nestDepth * 4);
-        let firstLines: string[] = specializations.reduce((lines, node, i) => {
-            let nextLevel = node.specializations;
-            let isLeaf = nextLevel.length === 0;
-            let id = makePatternIdentifier(node.pattern);
-            let condition = `${indent}${i > 0 ? 'else ' : ''}if (matches_${id}(address)) `;
-
-            // let condition = `${indent}${i > 0 ? 'else ' : ''}if (matches_${id}(address)) `;
-            // let consequent = isLeaf ? `return _${id};\n` : `{\n${getBody(nextLevel, spec.pattern, nestDepth + 1)}${indent}}\n`; // TODO: shorten to <120
-            // return condition + consequent;
-
-
-
-            if (isLeaf) {
-                let result = lines.concat(`${condition}return _${id};`);
-                return result;
-            }
-
-            let result = lines.concat([
-                `${condition}{`,
-                ...getBodyLines(nextLevel, node.pattern, nestDepth + 1),
-                `${indent}}`
-            ]);
-            return result;
-        }, <string[]> []);
-        let lastLine = `${indent}return _${makePatternIdentifier(fallback)};`;
-        return firstLines.concat(lastLine);
-    }
-
-    // TODO: doc...
     let lines = [
         ...patterns.map((pat, i) => `let matches_${makePatternIdentifier(pat)} = patterns[${i}].match;`),
         ...patterns.map((pat, i) => `let _${makePatternIdentifier(pat)} = targets[${i}];`),
@@ -65,4 +34,27 @@ export default function makeDispatcher<T>(taxonomy: Taxonomy, targetMap: Map<Pat
 // console.log(fn.toString());
 // debugger;
     return fn;
+}
+
+
+
+
+
+// TODO: doc...
+function getBodyLines(specializations: TaxonomyNode[], fallback: Pattern, nestDepth: number) {
+    let indent = ' '.repeat(nestDepth * 4);
+    let lines: string[] = [];
+    specializations.forEach((node, i) => {
+        let id = makePatternIdentifier(node.pattern);
+        let condition = `${indent}${i > 0 ? 'else ' : ''}if (matches_${id}(address)) `;
+        let nextLevel = node.specializations;
+        if (nextLevel.length === 0) return lines.push(`${condition}return _${id};`);
+        lines.push.apply(lines, [
+            `${condition}{`,
+            ...getBodyLines(nextLevel, node.pattern, nestDepth + 1),
+            `${indent}}`
+        ]);
+    });
+    lines.push(`${indent}return _${makePatternIdentifier(fallback)};`);
+    return lines;
 }
