7562d870167645c547df4792747da8970fc45a55
yortus <yortus@gmail.com>
2016-03-20 11:15:49 +0800

    WIP experimental routing helper functions

--- test/srvr/web-server.ts
+++ test/srvr/web-server.ts
@@ -1,17 +1,70 @@
 'use strict';
+import * as assert from 'assert';
 import * as http from 'http';
-import {makeHttpListener, RuleSet} from 'routist';
+import * as path from 'path';
+import {makeHttpListener, RuleSet, UNHANDLED} from 'routist';
+let stackTrace = require('stack-trace');
+
+
+
+
+
+function F(strings: string[], ...values: any[]) {
+
+    // TODO: temp just for now... implement more general approach...    
+    assert(strings.length === 1);
+    assert(values.length === 0);
+
+    // TODO: ...
+    let relPath = strings[0];
+    if (relPath.charAt(0) !== '.') return relPath;
+
+    // TODO: ...
+    let callerFilename = stackTrace.get()[1].getFileName();
+    let callerDirname = path.dirname(callerFilename);
+    let absPath = path.join(callerDirname, relPath);
+    return absPath;
+}
+
+
+
+
+
+// TODO: ...
+function file(absPath: string) {
+    return () => ({file: absPath});
+}
+function json(obj: any) {
+    return () => ({json: obj});
+}
+function html(str: string) {
+    return () => ({html: str});
+}
+function bundle(x) {
+    return ($next) => {
+        debugger;
+
+        let res = $next();
+        return res === UNHANDLED ? {json: [x]} : {json: [x].concat(res.json)};
+    };
+}
 
 
 
 
 
 let ruleSet = new RuleSet({
-    '/foo': () => ({html: 'foo'}),
-    '/bar': () => ({json: {bar: 'bar'}}),
-    '/lodash.js': () => ({file: `V:/oss/routist/node_modules/lodash/lodash.js`}),
-    '/lodash-all.js': () => ({bundle: `V:/oss/routist/node_modules/lodash/_*.js`}),
-    '...': () => ({html: 'fallback'})
+    'GET /foo':             html('foo'),
+    'GET /bar':             json({bar: 'bar'}),
+    'GET /lodash.js':       file(F`../../../node_modules/lodash/lodash.js`),
+    'GET /lodash-all.js':   () => ({bundle: `V:/oss/routist/node_modules/lodash/_*.js`}),
+
+    'GET /aaa #1': bundle('111'),
+    'GET /aaa #2': bundle('222'),
+
+
+
+    '...':                  html('fallback')
 });
 let httpListener = makeHttpListener(ruleSet);
 
