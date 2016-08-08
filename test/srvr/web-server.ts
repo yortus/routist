8be32eb26eedd077d52d238345a1dafd2e8c7f3c
Troy Gerwien <yortus@gmail.com>
2016-08-08 10:06:52 +0800

    add TODO comments

--- test/srvr/web-server.ts
+++ test/srvr/web-server.ts
@@ -32,3 +32,26 @@ let httpListener = makeHttpListener(ruleSet);
 let httpServer = http.createServer(httpListener);
 httpServer.listen(1337);
 console.log(`Web server listening on port ${1337}`);
+
+
+
+
+
+let ruleSet2 = {
+
+    // TODO: note to self after talking with Carl (8/8/2016)
+
+    // (1) can't express this: (but why in practice would you do this? is this a realistic use case?)    
+    'GET /*.js': html('*.js'), // catch all .js files
+    'GET /test.*': html('test.*'), // catch all test files
+    'GET /test.js': html('test.js'), // cover the overlap, but it doesn't cover the overlap b/c there's also test.*.js matching both prev patterns
+    //'GET /test.*.js': html('test.js'), // adding this doesn't solve the problem, because Pattern#interect just can't intersect the first two patterns
+
+
+    // (2) this works, but crashes only when an ambigious route like aaa/bbb.js is *accessed*
+    // wouldn't it be better if we worked out at the time of route table construction that there
+    // are ambiguous overlaps and report them early (and how to fix it by adding a route)? Or at least a switch?
+    'GET /aaa/...': html('*ab'), // catch everything starting with /aaa/...
+    'GET /....js': html('a/b'),   // catch all .js files
+    
+};
