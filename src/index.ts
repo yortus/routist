9b812cecae9a70aee0dbf117a5bc1d540106fbe3
yortus <yortus@gmail.com>
2018-03-19 15:02:45 +0800

    export Permission type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {deny, grant} from './access-control-helpers';
+import {Permission} from './access-control-types';
 import {createApplication, start, stop} from './express/application';
 import {user} from './identity-helpers';
 import Request from './request';
@@ -11,6 +12,6 @@ import {rpcMethods, staticFile, staticFiles} from './route-dispatch-helpers';
 
 export {createApplication as createExpressApplication};
 export {start, stop};
-export {grant, deny, user};
+export {grant, deny, Permission, user};
 export {Request, Response};
 export {rpcMethods, staticFile, staticFiles};
