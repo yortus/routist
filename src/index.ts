915194aa5b63b4485231389f52e91731a31c3f7f
yortus <yortus@gmail.com>
2018-05-23 15:59:46 +0800

    export more types and values
    - CONTINUE, RouteTable, RouteHandler, etc

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import {deny, grant} from './access-control-helpers';
-import {Permission} from './access-control-types';
+import {AccessGuard, AccessTable, Permission} from './access-control-types';
 import {createApplication, RoutistExpressApplication, start, stop} from './express/application';
 import {user} from './identity-helpers';
 import Request from './request';
 import Response from './response';
 import {rpcMethods, staticFile, staticFiles} from './route-dispatch-helpers';
-
+import {CONTINUE, RouteHandler, RouteTable} from './route-dispatch-types';
 
 
 
 
 export {createApplication as createExpressApplication, RoutistExpressApplication};
 export {start, stop};
-export {grant, deny, Permission, user};
+export {grant, deny, Permission, AccessGuard, AccessTable, user};
 export {Request, Response};
 export {rpcMethods, staticFile, staticFiles};
+export {CONTINUE, RouteHandler, RouteTable};
